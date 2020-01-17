79e7d79f30b28946988b7b2633b78851fc6405da
Nilrajsinh <Nilrajsinh1@icloud.com>
2020-01-17 13:49:26 +0530

    Initial Commit

---
+++ CoreDataLoginSignup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataLoginSignup
+//
+//  Created by Nilrajsinh Vaghela on 17/01/20.
+//  Copyright © 2020 Nilrajsinh Vaghela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
